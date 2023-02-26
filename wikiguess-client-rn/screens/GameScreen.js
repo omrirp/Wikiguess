3f7ecd799a0bba095b47000cfca01f5bbeca19a8
omrirp <omrirp@gmail.com>
2023-02-26 17:42:56 +0200

    game screen

--- wikiguess-client-rn/screens/GameScreen.js
+++ wikiguess-client-rn/screens/GameScreen.js
@@ -5,46 +5,60 @@ import Question from '../components/game/Question';
 import PrimaryButton from '../components/ui/PrimaryButton';
 
 const dataJason = [
-    { name: 'avi', age: 28, grade: 85, gender: 'male' },
-    { name: 'benny', age: 27, grade: 90, gender: 'male' },
-    { name: 'shmulik', age: 27, grade: 96, gender: 'male' },
-    { name: 'rivka', age: 25, grade: 80, gender: 'female' },
-    { name: 'mazal', age: 28, grade: 85, gender: 'female' },
+    { name: 'avi', age: 28, grade: 90, gender: 'male' },
+    { name: 'dani', age: 25, grade: 90, gender: 'male' },
+    { name: 'benny', age: 27, grade: 87, gender: 'male' },
+    { name: 'shmulik', age: 26, grade: 85, gender: 'male' },
+    { name: 'rivka', age: 25, grade: 70, gender: 'female' },
+    { name: 'mazal', age: 28, grade: 95, gender: 'female' },
 ];
 
 export default function GameScreen() {
     const [questionNum, setQuestionNum] = useState(1);
-    const [lastAnswer, setLastAnswer] = useState('yes');
     const [question, setQuestion] = useState('is your Charectes gender is male?');
     const [data, setData] = useState(dataJason);
+    const [lastAnswer, setLastAnswer] = useState('yes');
 
-    useEffect(() => {}, []);
-
+    // Decide what unique value to use for renderind the question
     function decision() {
         let probabilities = {};
+        // Asumeing all object have the same attributes
+        let keys = Object.keys(data[0]);
 
-        // Find the probabilities for each of the unique values within the
-        // Result for probabilities- {unique valeu: probability, ...}
-        for (let i = 0; i < data.length; i++) {
-            if (!probabilities[data[i][key]]) {
-                probabilities[data[i][key]] = data.filter((o) => o[key] === data[i][key]).length / data.length;
+        // Iterating on every key (column) in the data
+        keys.forEach((key) => {
+            // Find the probabilities for each of the unique value in the data
+            for (let i = 0; i < data.length; i++) {
+                if (!probabilities[data[i][key]]) {
+                    let probability = data.filter((o) => o[key] === data[i][key]).length / data.length;
+                    probabilities[data[i][key]] = { probability, key };
+                }
             }
-        }
+        });
+        // Result for probabilities- {unique valeu: probability, ...}
+        //console.log(probabilities);
 
-        // Find the unique value that will be to closest to cut the data to 50%
+        // Find the unique value that will be closest to cut the data to 50%
+        // meaning |probability(value)-0.5| will give the minimum result
         let probKeys = Object.keys(probabilities);
         let toAsk;
         let cut = 1;
         probKeys.forEach((p) => {
-            let calc = Math.abs(probabilities[p] - 0.5);
+            let calc = Math.abs(probabilities[p].probability - 0.5);
             if (cut > calc) {
                 cut = calc;
-                toAsk = p;
+                toAsk = [probabilities[p].key, p];
             }
         });
-
+        // Result for toAsk- [name of the key, name of the unique value]
         return toAsk;
     }
+
+    useEffect(() => {
+        //let entropyObj = calculateEntropy(data);
+        let [key, value] = decision();
+        setQuestion('is you charectar ' + key + ' is ' + value + '?');
+    }, [data]);
 
     return (
         <View style={styles.rootContainer}>
